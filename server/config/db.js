8a5233412a9343d84eba7f67b509019b1d291d4c
Ketan kumar <ketankumar147856@gmail.com>
2026-05-08 07:00:37 +0000

    fix update

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDb=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://ketanrs59_db_user:ketan59@cluster0.fmplo3s.mongodb.net/?appName=Cluster0");
         console.log("Databse connected succesfully");
     }catch(error){
         console.error("DB Error:", error.message);
